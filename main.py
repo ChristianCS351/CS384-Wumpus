000deb7293e587bae768a354daeab296d832e18d
Christian <cookce14@mycu.concord.edu>
2025-05-09 11:55:47 -0400

    Making my rules menu for stuff to work with the main menu.

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import random
 
-
-
-
-
 def menu():  #This is my menu for the Wumpus game, I plan to make a few gamemodes here to make it unique and a rules sheet.
     print("Welcome to Hunt the Wumpus!")
     print("---------------------------/n/n")
@@ -13,7 +9,7 @@ def menu():  #This is my menu for the Wumpus game, I plan to make a few gamemode
     print("-------3. Play Hard Mode: -----------")
     print("-------4. Read Rules: ---------------")
     print("-------5. Exit Game: ----------------\n\n")
-    game_choice = input("Please select an option between (1 to 4):")
+    game_choice = input("Please select an option between (1 to 5):")
 
     if game_choice == '1':
         print("\nOk then, let's bring you into the classic version of the Hunt the Wumpus Game!\n")
@@ -27,12 +23,40 @@ def menu():  #This is my menu for the Wumpus game, I plan to make a few gamemode
     elif game_choice == '4':
         print("\nOk then, Goodbye :D")
         rules_sheet()
-    elif game_choice == '5'
+    elif game_choice == '5':
         print("\nOk then, Goodbye :D")
         sys.exit
     else:
-        print("Invalid Choice, please type 1,2,3,4,5 in the menu.\n")
+        print("Invalid Choice, please type 1,2,3,4,5 in the game menu.\n")
     
+
+def rules_sheet():
+    print("Welcome to the Rules Sheet, Your Guide to Know How to Play:")
+    print("\nOk, what would you like to learn about? Just Select the Number Options:")
+    print("-------1. General Rules: ---------")
+    print("-------2. Normal Mode Rules: -----------")
+    print("-------3. Baby Mode Rules -----------")
+    print("-------4. Hard Rules: ---------------")
+    print("-------5. Return to Menu: ----------------\n\n")
+    menu_choice = input("Please select an option between (1 to 5):")
+
+    if menu_choice == '1':
+        print("\nHere are the General Rules!")
+        print("----------------------")
+    elif menu_choice == '2':
+        print("\nHere are the Normal Game Mode Rules!")
+        print("----------------------")
+    elif menu_choice == '3':
+        print("\nHere are the Baby Game Mode Rules!")
+        print("----------------------")
+    elif menu_choice == '4':
+        print("\nHere are the Hard Game Mode Rules!")
+        print("----------------------")
+    elif menu_choice == '5':
+        print("\nReturning to menu!\n")
+        sys.exit
+    else:
+        print("Invalid Choice, please type 1,2,3,4,5 in the rules menu.\n")
    
 
 
