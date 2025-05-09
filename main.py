aa60c20c177aa6ea1281cd511e21bdcdfb9b4cbf
Christian <cookce14@mycu.concord.edu>
2025-05-09 11:36:14 -0400

    imported random and I am working on menu selection for game.

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+
+
+
+
+def menu():  #This is my menu for the Wumpus game, I plan to make a few gamemodes here to make it unique and a rules sheet.
+    print("Welcome to Hunt the Wumpus!")
+    print("---------------------------/n/n")
+    print()
+    game_choice = input("Please select an option between (1 to 4):")
+   
+
+
